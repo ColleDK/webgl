e76daba6be622fcfdd030ee7aaeac297eab3d431
= <wimo@boozt.com>
2022-08-31 10:51:36 +0200

    P3 and start P4

--- lektion1/P2/W01P2.js
+++ lektion1/P2/W01P2.js
@@ -1,7 +1,7 @@
 window.onload = function init(){
     var canvas = document.getElementById("c");
     var gl = canvas.getContext("webgl");
-    gl.clearColor(1.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
---
+++ lektion1/P4/W01P4.js
@@ -0,0 +1,40 @@
+window.onload = function init(){
+    var canvas = document.getElementById("c");
+    var gl = canvas.getContext("webgl");
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    console.log("Program is ", program)
+    gl.useProgram(program);
+
+    // Define object data
+    var vertices = [ vec2(-0.5, -0.5), vec2(0.5, -0.5), vec2(0.5, 0.5), vec2(-0.5, -0.5), vec2(-0.5, 0.5), vec2(0.5, 0.5) ];
+    var colors = [vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0), vec4(1.0, 1.0, 1.0, 1.0)]
+    
+    // Create data buffers
+    var vBuffer = gl.createBuffer();
+    var cBuffer = gl.createBuffer();
+
+    // Bind position buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    
+    // Get position attribute location
+    var vPosition = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+
+    // Bind color buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    // Get color attribute position
+    var vColor = gl.getAttribLocation(program, "aColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    // Draw object
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
