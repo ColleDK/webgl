6e4d3593e8aa4f57a31eade3174eebf52108920f
William Mortensen <wimo@boozt.com>
2022-09-07 10:51:21 +0200

    Finished w2p1 and started w2p2

--- lektion2/P2/W02P2.js
+++ lektion2/P2/W02P2.js
@@ -1,6 +1,9 @@
 window.onload = function init(){
     var canvas = document.getElementById("c");
+    var colorSelector = document.getElementById("colorMenu");
+
     var gl = canvas.getContext("webgl");
+
     gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -9,11 +12,20 @@ window.onload = function init(){
     gl.useProgram(program);
 
     // Define object data
+    var colors = [
+        vec4(0.0, 0.0, 0.0, 1.0), // Black
+        vec4(1.0, 0.0, 0.0, 1.0), // Red
+        vec4(0.0, 1.0, 0.0, 1.0), // Green
+        vec4(0.0, 0.0, 1.0, 1.0), // Blue
+        vec4(1.0, 1.0, 1.0, 1.0), // White
+    ]
     var maxVertices = 1000;
     var index = 0; var numPoints = 0;
 
     // Click listener
     canvas.addEventListener("click", (event) => {
+        var currentColor = colors[colorSelector.selectedIndex];
+        console.log("Current color " + flatten(currentColor))
 
         // Get mouse position in draw area
         var bbox = event.target.getBoundingClientRect();
@@ -23,7 +35,8 @@ window.onload = function init(){
         )
 
         // Add the position to the buffer data
-        gl.bufferSubData(gl.ARRAY_BUFFER, index * sizeof['vec2'], flatten(mousepos))
+        gl.bufferSubData(gl.ARRAY_BUFFER, index * sizeof['vec2'], flatten(mousepos));
+        gl.bufferSubData(gl.ARRAY_BUFFER, index * sizeof['vec4'], flatten(currentColor));
 
         numPoints = Math.max(numPoints, ++index); index %= maxVertices;
     })
@@ -43,16 +56,13 @@ window.onload = function init(){
 
 
     // Bind color buffer
-    //gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    //gl.bufferData(gl.ARRAY_BUFFER, maxVertices * sizeof['vec4'], gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, maxVertices * sizeof['vec4'], gl.STATIC_DRAW);
 
     // Get color attribute position
-    var vColor = gl.getUniformLocation(program, "aColor");
-    var color = vec4(0.0, 0.0, 0.0, 1.0);
-    gl.uniform4fv(vColor, flatten(color));
-
-    //gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    //gl.enableVertexAttribArray(vColor);
+    var vColor = gl.getAttribLocation(program, "aColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
 
     // Draw object
     //gl.drawArrays(gl.TRIANGLES, 0, 3);
